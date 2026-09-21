1edc366395f9a2dd13858acee72ed25b5d639769
6947dastanbek-lang <6947dastanbek@gmail.com>
2026-09-22 00:07:53 +0900

    feat:Inital Setup

--- src/server.ts
+++ src/server.ts
@@ -9,3 +9,16 @@ console.log(time);
 
 const person: string = "Ali";
 const count: number = 29;
+
+// Architectural pattern: MVC, DI, MVP. //(butun application'ni qanday katta qismlarga bo‘lib tashkil qilish usuli)
+// MVC ✅ ⬇️
+// Model (data/database bilan ishlaydigan qism)
+// View (foydalanuvchiga ko‘rinadigan qism)
+// Controller (requestni qabul qilib, nima qilish kerakligini boshqaradigan qism)
+
+// Design pattern: Middleware, Decorator  //(kod ichidagi ma'lum bir muammoni yechish uchun ishlatiladigan tayyor/tavsiya etilgan struktura)
+/*Middleware ✅
+(request va response orasida ishlaydigan qo‘shimcha funksiya)
+
+Decorator ✅
+(class yoki methodga qo‘shimcha xususiyat/xatti-harakat qo‘shish usuli) */
