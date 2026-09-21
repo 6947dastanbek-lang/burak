c90916e66cb40d29105238de2b6882dca89d2b6c
6947dastanbek-lang <6947dastanbek@gmail.com>
2026-09-21 23:39:15 +0900

    feat:Inital Setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+console.log("EXECUTED");
+
+// setInterval(() => {
+//     console.log("RUNNING");
+// }, 1000);
+import moment from "moment";
+const time = moment().format("YYYY MM DD");
+console.log(time);
+
+const person: string = "Ali";
+const count: number = 29;
