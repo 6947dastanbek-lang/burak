8cb2f608abc7d380cd7292c0532120f9d28babfd
6947dastanbek-lang <6947dastanbek@gmail.com>
2026-09-24 16:10:05 +0900

    feat:express integrations ✅  and M-TASK M-TASK is done ✅

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,32 @@
 import dotenv from "dotenv";
-dotenv.config();
+// dotenv → .env faylidagi environment variable'larni o‘qish uchun ishlatiladi
 
-// console.log("PORT:", process.env.PORT);
-// console.log("Mongo_url:", process.env.MONGO_URL);
+dotenv.config();
+// .env ichidagi qiymatlarni process.env ichiga yuklaydi
+
+import mongoose from "mongoose";
+// mongoose → MongoDB bilan ishlash uchun library
+
+import app from "./app";
+// ./app → app.ts faylidan app obyektini olib keladi
+
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    // as string → TypeScript'ga "bu qiymat string" deb aytyapmiz
+    .then((data) => {
+        // data → ulanishdan qaytgan ma'lumot
+        console.log("MongoDB connection succeed");
+        const PORT = process.env.PORT ?? 3003;
+        // ?? → agar PORT null yoki undefined bo‘lsa
+        // 3003 → default qiymat sifatida ishlatiladi
+        app.listen(PORT, function () {
+            // app.listen() → serverni ishga tushiradi
+            // PORT → server qaysi portda ishlashini bildiradi
+            // function() → server ishga tushgandan keyin bajariladi
+
+            console.log(`The server is running successfully on port: ${PORT}`);
+            // server muvaffaqiyatli ishga tushganini ko‘rsatadi
+        });
+    })
+
+    .catch((err) => console.log("ERROR on connection MongoDB", err));
--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,18 @@
-console.log("HELLO WORLD");
+//MITASK
+//M-TASK
+
+function getSquareNumbers(
+    numbers: number[],
+): { number: number; square: number }[] {
+    // function objectlardan iborat array qaytaradi
+
+    return numbers.map((number) => ({
+        // arraydagi HAR BIR number new object
+
+        number: number,
+
+        square: number ** 2,
+    }));
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
